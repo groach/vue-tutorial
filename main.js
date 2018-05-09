33a0f77fa9d7d68d676a8faf022c50bc14c93293
groach <garrett@webstreak.com>
2018-05-09 16:10:11 -0400

    Lesson 5

--- main.js
+++ main.js
@@ -8,15 +8,31 @@ var app = new Vue({
     inventory: 0,
     onSale: true,
     details: ['det1', 'det2', 'det3'],
+    cart: 0,
     variants: [
       {
         vId: 3333,
-        color: 'green'
+        color: 'green',
+        img: './assets/img.png'
       },
       {
         vId: 4444,
-        color: 'red'
+        color: 'red',
+        img: './assets/img2.png'
       }
     ]
+  },
+  methods: {
+    addToCart: function(){
+      this.cart += 1
+    },
+    removeFromCart: function(){
+      if (this.cart > 0){
+        this.cart -= 1
+      }
+    },
+    updateProduct: function(img){
+      this.image = img
+    }
   }
 })
