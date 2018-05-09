1da0c445ac0ee302afe35fa09b84efedecd205e4
groach <garrett@webstreak.com>
2018-05-09 15:39:09 -0400

    Lesson 2

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'a description'
+    description: 'a description',
+    image: './assets/img.png',
+    link: 'htts://google.com'
   }
 })
