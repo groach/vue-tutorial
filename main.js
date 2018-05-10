ae9ff30f95a10740c320201a897ab84994573f05
groach <garrett@webstreak.com>
2018-05-10 11:52:14 -0400

    Lesson 7

--- main.js
+++ main.js
@@ -2,23 +2,26 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
+    brand: 'Brand Name',
     description: 'a description',
-    image: './assets/img.png',
+    selectedVariant: 0,
     link: 'htts://google.com',
-    inventory: 0,
-    onSale: true,
     details: ['det1', 'det2', 'det3'],
     cart: 0,
     variants: [
       {
         vId: 3333,
         color: 'green',
-        img: './assets/img.png'
+        img: './assets/img.png',
+        qty: 0,
+        sale: true
       },
       {
         vId: 4444,
         color: 'red',
-        img: './assets/img2.png'
+        img: './assets/img2.png',
+        qty: 30,
+        sale: false
       }
     ]
   },
@@ -31,8 +34,22 @@ var app = new Vue({
         this.cart -= 1
       }
     },
-    updateProduct: function(img){
-      this.image = img
+    updateProduct: function(index){
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title: function(){
+      return this.brand + ' ' + this.product
+    },
+    image(){
+      return this.variants[this.selectedVariant].img
+    },
+    inventory(){
+      return this.variants[this.selectedVariant].qty
+    },
+    onSale(){
+      return this.variants[this.selectedVariant].sale
     }
   }
 })
